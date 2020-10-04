3d54ce1dbac00430ddd1619a75e391eda69dfdf0
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2020-10-04 15:24:22 +0200

    Updates

---
+++ precache-manifest.10c4f663435e6510698d10294b81b514.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24511d92e7481bcfabd5ee06fc628b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3e2ede454352b53fc68",
+    "url": "/static/js/2.a9366fbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a9366fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a53a3fadcc8dea0748",
+    "url": "/static/js/main.38604290.chunk.js"
+  },
+  {
+    "revision": "cd42b7a38cc9775f94ee",
+    "url": "/static/js/runtime-main.f87d05e3.js"
+  }
+]);
